871f70a444b1ab459c241cedc6f6ba9467e54f80
rebelcolony <kevin@rebelcolony.com>
2012-01-28 16:45:50 +0100

    git initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smb::Application.config.secret_token = 'eeab990011cd2915e55ac7caa48713b4e1793d1ce040268f56a04f25297acb879e9ef17124c043f5458f8d84f960f299d631b0d5cae04ac7cb89f13e87220bd0'
