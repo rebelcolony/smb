6a734fce9d8a3ec34cbae93da985d357e64401cf
rebelcolony <kevin@rebelcolony.com>
2012-08-30 20:41:27 +0200

    added published date, footer & login edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120807062707) do
+ActiveRecord::Schema.define(:version => 20120830154046) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120807062707) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "image"
+    t.date     "published"
   end
 
 end
